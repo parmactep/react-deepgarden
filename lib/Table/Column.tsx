dbb7ddb704c09804178f3dbf073ecaa0dfe4f421
Par Mactep <parmactep@gmail.com>
2023-05-01 02:30:30 +0300

    Table TS

---
+++ lib/Table/Column.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+import classNames from 'classnames';
+
+export interface IColumnProps {
+	className?: string;
+	children?: ReactNode;
+	width?: number;
+	title?: ReactNode;
+	summary?: ReactNode;
+}
+
+const Column = ({ width, title, className }: IColumnProps) => (
+	<div
+		className={classNames('_Table__Column', className)}
+		style={width && { width }}
+	>
+		{title}
+	</div>
+);
+
+Column.displayName = 'Table.Column';
+
+export default Column;
--- lib/Table/Table.tsx
+++ lib/Table/Table.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
 
+interface ITableProps {
+	className?: string;
+}
+
 const Table = ({
 	className,
 	...props
-}) => (
+}: ITableProps) => (
 	<div
 		className={classNames('_Table__Table', className)}
 		{...props}
