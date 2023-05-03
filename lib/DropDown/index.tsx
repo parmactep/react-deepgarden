44e8f19bb56e73784fab323ffab2e7c073ecc6bc
Par Mactep <parmactep@gmail.com>
2023-05-03 23:00:04 +0300

    Fix dropDown

--- lib/DropDown/index.tsx
+++ lib/DropDown/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import classNames from 'classnames';
 
@@ -11,14 +11,29 @@ const DIRECTION_CLASS: Record<string, string> = {
 	left_top: 'LeftTop',
 };
 
-function DropDown() {
+interface IDropDownProps {
+	direction?: keyof typeof DIRECTION_CLASS;
+	className?: string;
+	children?: ReactNode;
+	onClose?: (e: Event) => void;
+}
+
+function DropDown({
+	direction =  'bottom',
+	className,
+	children,
+	onClose,
+}: IDropDownProps) {
+	const handleClickOutside = (e: Event) => {
+		onClose && onClose(e);
+	};
 	return (
 		<OutsideClick
-			className={classNames('_DropDown', `_DropDown--${DIRECTION_CLASS[this.props.direction]}`, this.props.className)}
-			onClickOutside={this.handleClickOutside}
+			className={classNames('_DropDown', `_DropDown--${DIRECTION_CLASS[direction]}`, className)}
+			onClickOutside={handleClickOutside}
 		>
 			<div className="_DropDown__Body">
-				{this.props.children}
+				{children}
 			</div>
 		</OutsideClick>
 	);
