f0d7c99cc8c00f2967e8b8fd310502f313f666c8
Taras <dmutruk.taras@gmail.com>
2023-07-03 14:03:48 +0300

    TypeScript refactoring Grid component

---
+++ lib/Grid/Col.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react';
+import classNames from 'classnames';
+
+const alignClasses = {
+	start: '_Grid__Col--AlignStart',
+	end: '_Grid__Col--AlignEnd',
+	center: '_Grid__Col--AlignCenter',
+};
+
+export interface IColProps {
+	className?: string;
+	col?: number;
+	offset?: number;
+	align?:	keyof typeof alignClasses;
+	children?: ReactNode;
+}
+
+export default ({ className, col, offset, align, children }: IColProps) => {
+	const colClass = col ? `_Grid__Col--${col}` : '';
+	const offsetClass = offset ? `'_Grid__Col--Offset' ${offset}` : '';
+	const alignClass = align ? alignClasses[align] : '';
+
+	return (
+		<div className={classNames('_Grid__Col', colClass, offsetClass, alignClass, className)}>
+			{children}
+		</div>
+	);
+};
